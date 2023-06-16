2e6db61b6f3f5701dcf2061641fa39f0e26b6d08
Ivan Lozic <ivanlozic995@gmail.com>
2023-06-17 00:16:40 +0200

    Home page icons changed

--- src/pages/aboutPage/index.tsx
+++ src/pages/aboutPage/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Menu from '@/components/menu/Menu';
 import useMenu from '@/hooks/useMenu';
+import Image from 'next/image';
 
 const AboutPage = () => {
   const { isMenuOpen, toggleMenu } = useMenu();
@@ -10,7 +11,11 @@ const AboutPage = () => {
     <div className="aboutPage">
       <Menu isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
         
-  
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere facilis consectetur eum ut sed quos neque officiis laudantium et dolore nemo suscipit, vero delectus eaque, adipisci accusamus quas maxime? Ducimus enim doloribus totam a, impedit fugit accusantium. Debitis voluptatum repellat soluta, ab delectus consequatur fugiat odio. Dolorem aliquam rerum ratione!</p>
+
+    
+
     </div>
   );
 };
